151834d8b3a462afb0ae0d0d3aef7930172fd6b0
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2022-01-13 22:32:47 -0500

    Contracts update

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-  contractAddress: "0x9704e539D741e5C59E29FaFBe6b4Ab271bd68FC1",
-  nftContractAddress: "0xCEE29418D1F6f8eC86C99FE754eB921923e5055B",
+  contractAddress: "0x2cC7Eb52182E9cE78f809B73864A1fe56029b7F3",
+  nftContractAddress: "0x615fDD500e5A0FFf4BAB4152B2774afa652e9346",
 };
 
 export default config;
